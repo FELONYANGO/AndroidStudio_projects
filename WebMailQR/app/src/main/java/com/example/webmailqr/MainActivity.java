4e0f1ff5e8a8abeb1636681d01615a13386fc0a4
FELONYANGO <felixonyango951@gmail.com>
2022-05-30 14:48:42 +0300

    created the first activity  main activity

---
+++ WebMailQR/app/src/main/java/com/example/webmailqr/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.webmailqr;
+
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.SurfaceTexture;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.Surface;
+import android.view.TextureView;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity{
+    VideoView videoView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        videoView = findViewById(R.id.videoView);
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.mobile);
+        videoView.setVideoURI(uri);
+        videoView.start();
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+
+                mp.setLooping(true);
+            }
+        });
+    
+    }
+
+    @Override
+    protected void onResume() {
+        videoView.resume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        videoView.start();
+        super.onRestart();
+    }
+
+    @Override
+    protected void onPause() {
+        videoView.suspend();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        videoView.stopPlayback();
+        super.onDestroy();
+    }
+}
+
---
+++ webMailQR/app/src/main/java/com/example/webmailqr/activity_scanned_barcode.java
@@ -0,0 +1,14 @@
+package com.example.webmailqr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_scanned_barcode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanned_barcode);
+    }
+}
